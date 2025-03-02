30608a4c24bb4aa5ce74ba6b75056b2b64f929ab
RaymondHornDev <132532625+RaymondHornDev@users.noreply.github.com>
2025-03-01 20:35:17 -0600

    Changes objects on singleton to static

--- Platformer/Project/platformer/Scripts/Autoload/GameMaster.gd
+++ Platformer/Project/platformer/Scripts/Autoload/GameMaster.gd
@@ -2,10 +2,10 @@ extends Node
 """ Singleton """
 
 # Enemy movement and state script
-var npc_object:NPC_Object = NPC_Object.new()
+static var npc_object:NPC_Object = NPC_Object.new()
 
 # References to active game objects
-var obj_ref:Object_References = Object_References.new()
+static var obj_ref:Object_References = Object_References.new()
 
 # Scene changing script
-var scene_change:SceneChangeObject = SceneChangeObject.new()
+static var scene_change:SceneChangeObject = SceneChangeObject.new()
